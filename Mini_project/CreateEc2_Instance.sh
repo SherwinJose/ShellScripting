cb5b439bb24befe239262f46df99300baed01c8f
Sherwinjose <shewyjose79@gmail.com>
2023-03-14 00:31:23 +0530

    Create_EC2_Instance

--- Mini_project/CreateEc2_Instance.sh
+++ Mini_project/CreateEc2_Instance.sh
@@ -9,3 +9,10 @@ if [ -z "${INSTANCE_NAME}" ]; then
 fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+
+if [ -z "${AMI_ID}" ]; then
+  echo -e "Unable to find Image AMI_ID"
+  exit
+  else
+    echo -e "\e[1;32mAMI ID = ${AMI_ID}\e[0m"
+  fi
