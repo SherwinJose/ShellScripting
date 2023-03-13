ad0d23b8aaa2dff6adcee6eef599a36fd33320b4
Sherwinjose <shewyjose79@gmail.com>
2023-03-14 00:30:25 +0530

    Create_EC2_Instance

--- Mini_project/CreateEc2_Instance.sh
+++ Mini_project/CreateEc2_Instance.sh
@@ -7,3 +7,5 @@ if [ -z "${INSTANCE_NAME}" ]; then
   echo -e "Instance Name Argument is needed"
   exit
 fi
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
