90a45982da909402e6bf79c2d86373ab48fc6f61
Sherwinjose <shewyjose79@gmail.com>
2023-03-14 00:50:50 +0530

    Create_EC2_Instance

--- Mini_project/CreateEc2_Instance.sh
+++ Mini_project/CreateEc2_Instance.sh
@@ -7,7 +7,6 @@ if [ -z "${INSTANCE_NAME}" ]; then
   echo -e "Instance Name Argument is needed"
   exit
 fi
-
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z "${AMI_ID}" ]; then
@@ -16,5 +15,4 @@ if [ -z "${AMI_ID}" ]; then
   else
     echo  "AMI ID = ${AMI_ID}"
   fi
-
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
