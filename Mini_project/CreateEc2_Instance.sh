7baa8ad5673bd06c5675045b0f2a7e139eccd2e0
Sherwinjose <shewyjose79@gmail.com>
2023-03-14 01:05:49 +0530

    Create_EC2_Instance

--- Mini_project/CreateEc2_Instance.sh
+++ Mini_project/CreateEc2_Instance.sh
@@ -15,3 +15,6 @@ if [ -z "${AMI_ID}" ]; then
     echo  "AMI ID = ${AMI_ID}"
   fi
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
+if [ -z "${PRIVATE_IP}" ]; then
+  SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=Devops_practice --query "SecurityGroups[*].GroupId" --output text)
+
