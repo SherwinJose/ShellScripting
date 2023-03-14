804a0ae143583ab8ea44cafb9c70d1716d90831f
Sherwinjose <shewyjose79@gmail.com>
2023-03-14 09:06:28 +0530

    Create_EC2_Instance

--- Mini_project/CreateEc2_Instance.sh
+++ Mini_project/CreateEc2_Instance.sh
@@ -16,7 +16,7 @@ if [ -z "${AMI_ID}" ]; then
   else
     echo  "AMI ID = ${AMI_ID}"
   fi
-aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
+PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 if [ -z "${PRIVATE_IP}" ]; then
   SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=Devops_practice --query "SecurityGroups[*].GroupId" --output text)
    if [ -z "${SG_ID}" ]; then
@@ -48,7 +48,5 @@ ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id
 aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json --output text &>>$LOG
 echo -e "\e[1m DNS Record Created\e[0m"
 
-fi
-  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"  --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"  &>>$LOG
-  echo -e "\e[1m Instance Created\e[0m"
+
 
