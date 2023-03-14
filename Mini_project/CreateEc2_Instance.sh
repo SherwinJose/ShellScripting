d2212bee0e93f89fafceef8df0a4891c87a69aa2
Sherwinjose <shewyjose79@gmail.com>
2023-03-14 09:25:14 +0530

    Create_EC2_Instance

--- Mini_project/CreateEc2_Instance.sh
+++ Mini_project/CreateEc2_Instance.sh
@@ -17,36 +17,21 @@ if [ -z "${AMI_ID}" ]; then
     echo  "AMI ID = ${AMI_ID}"
   fi
 PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+
 if [ -z "${PRIVATE_IP}" ]; then
   SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=Devops_practice --query "SecurityGroups[*].GroupId" --output text)
    if [ -z "${SG_ID}" ]; then
       echo "Security Group allow-all-ports does not exist"
       exit
-fi
+      fi
   aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"  --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"  &>>$LOG
-  echo -e "\e[1m Instance Created\e[0m"
+  echo  " Instance Created"
 else
   echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
 fi
+exit
 
 
-IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
-
-echo '{
-            "Comment": "CREATE/DELETE/UPSERT a record ",
-            "Changes": [{
-            "Action": "UPSERT",
-                        "ResourceRecordSet": {
-                                    "Name": "DNSNAME",
-                                    "Type": "A",
-                                    "TTL": 300,
-                                 "ResourceRecords": [{ "Value": "IPADDRESS"}]
-}}]
-}' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/"  >/tmp/record.json
-
-ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id}" --output text | grep roboshop.internal  | awk '{print $1}' | awk -F / '{print $3}')
-aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json --output text &>>$LOG
-echo -e "\e[1m DNS Record Created\e[0m"
 
 
 
