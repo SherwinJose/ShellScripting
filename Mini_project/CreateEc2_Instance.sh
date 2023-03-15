a3bb269bce758ec59e5f71d8757b6b3c9a51feed
Sherwinjose <shewyjose79@gmail.com>
2023-03-16 00:17:11 +0530

    Create_EC2_Instance

--- Mini_project/CreateEc2_Instance.sh
+++ Mini_project/CreateEc2_Instance.sh
@@ -50,6 +50,7 @@ echo '{
                                     "TTL": 300,
                                  "ResourceRecords": [{ "Value": "IPADDRESS"}]
 }}]
+}' | sed -e "s/DNSNAME/${INSTANCE_NAME}/" -e "s/IPADDRESS/${IPADDRESS}/"  >/tmp/record.json
 
 
 
