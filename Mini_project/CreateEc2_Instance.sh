d2383572120c1ecad5a51bd84b611fe37f3d3b4d
Sherwinjose <shewyjose79@gmail.com>
2023-03-16 21:43:43 +0530

    Create_EC2_Instance

--- Mini_project/CreateEc2_Instance.sh
+++ Mini_project/CreateEc2_Instance.sh
@@ -4,33 +4,32 @@
 LOG=/tmp/instance-create.log
 rm -f $LOG
 
-if [ "$1" == "list" ]; then
+if [ "$1" == "list" ]; then #if the first arguments is givens as list
   aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  --output table
-  exit
+  exit                                                                                                                             ##The Above AWS command will Gives us the detailed information about the EC2 instances that are running with status and their public and private IPs
 fi
 
 INSTANCE_NAME=$1
-if [ -z "${INSTANCE_NAME}" ]; then
+if [ -z "${INSTANCE_NAME}" ]; then                                                                                                 ##if instance name is passed as a arguments or not validation
   echo -e "Instance Name Argument is needed"
   exit
 fi
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text) ##aws command for getting the AMI_ID for the customAMI Centos-7-DevOps-Practice
 if [ -z "${AMI_ID}" ]; then
   echo  "Unable to find Image AMI_ID"
   exit
   else
     echo  "AMI = ${AMI_ID}"
   fi
-PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+PRIVATE_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)   ##aws command for fetching the private IP of the
 
 if [ -z "${PRIVATE_IP}" ]; then
+  # Find Security Group
   SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=Devops_practice --query "SecurityGroups[*].GroupId" --output text)
-
-     echo
-   if [ -z "${SG_ID}" ]; then
-      echo "Security Group allow-all-ports does not exist"
+if [ -z "${SG_ID}" ]; then
+      echo "Security Group Devops_practice does not exist"
       exit
-      fi
+ fi
   aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"  --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"  &>>$LOG
   echo  " Instance Created"
 else
@@ -56,7 +55,9 @@ ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,ID:Id
 
 aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/record.json --output text &>>$LOG
 
-echo -e "\e[1m DNS Record Created\e[0m"
+echo - "DNS Record Created"
+
+
 
 
 
