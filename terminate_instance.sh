4a03bb680596852af65a3ed1f4d08445aece0583
Sherwinjose <shewyjose79@gmail.com>
2023-03-29 13:51:49 +0530

    Ansible

--- terminate_instance.sh
+++ terminate_instance.sh
@@ -1,13 +1,13 @@
 LOG=/tmp/terminate_instance.log
 rm -f $LOG
 
-PRIVATE_IP=$1
-  if [ -z "${PRIVATE_IP}" ]; then
-    echo -e "\e[1;33mPRIVATE_IP Argument is needed\e[0m"
+INSTANCE_NAME=$1
+  if [ -z "${INSTANCE_NAME}" ]; then
+    echo -e "\e[1;INSTANCE_NAME Argument is needed\e[0m"
     exit
   fi
 
-INSTANCE_ID=$(aws ec2 describe-instances --filter Name=private-ip-address,Value=$PRIVATE_IP --'Reservations[*].Instances[*].InstanceID' --output text )
+INSTANCE_ID=$(aws ec2 describe-instances --filter "Name=tag:Name,Values=${INSTANCE_NAME}" --query 'Reservations[*].Instances[*].InstanceID' --output text )
 
 echo "InstanceID =  ${INSTANCE_ID} "
 
